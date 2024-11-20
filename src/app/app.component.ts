312ac5c21b8f121b52911438dd10c2ff63b07a78
Alnabeel-acsgithub <alnabeel@agilecyber.com>
2024-11-20 10:32:11 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ReactiveFormsModule,MatInputModule,MatCardModule,MatButtonModule,MatToolbarModule],  // Add ReactiveFormsModule here
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  toggleForm = signal(true)
   title = 'dynamic-form';
+  genderOption = ["Male","Female"]
+  // Create a reactive form group with a single 'name' control
+  userForm = new FormGroup({
+    name: new FormControl('', [Validators.minLength(3),Validators.required]), // The 'name' control
+    gender : new FormControl ("male",[Validators.required])
+  });
+  onSubmit (){
+    const value = this.userForm.value;
+    console.log(value,"ll");
+    
+  }
+  formToggle() {
+    this.toggleForm.set(!this.toggleForm())
+  }
 }
